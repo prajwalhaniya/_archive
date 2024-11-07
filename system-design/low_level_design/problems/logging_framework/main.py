127a27fc600f0a4757bd9404a1cbe46e9e72dcc2
prajwalhaniya <prajwalhaniya@gmail.com>
2024-11-07 23:17:49 +0530

    archive update

---
+++ system-design/low_level_design/problems/logging_framework/main.py
@@ -0,0 +1,40 @@
+from enum.log_level import LogLevel
+
+class Logger:
+    _instance = None
+
+    def __init__(self):
+        if Logger._instance is not None:
+            raise Exception("This class is a singleton")
+        else:
+            Logger._instance = self
+            self.config = LoggerConfig(LogLevel.INFO, ConsoleAppender())
+    
+    @staticmethod
+    def get_instance():
+        if Logger._instance is None:
+            Logger()
+        return Logger._instance
+    
+    def set_config(self, config):
+        self.config = config
+
+    def log(self, level, message):
+        if level.value >= self.config.get_log_level().value:
+            log_message = LogMessage(level, message)
+            self.config.get_log_appender().append(log_message)
+    
+    def debug(self, message):
+        self.log(LogLevel.DEBUG, message)
+
+    def info(self, message):
+        self.log(LogLevel.INFO, message)
+
+    def warning(self, message):
+        self.log(LogLevel.WARNING, message)
+
+    def error(self, message):
+        self.log(LogLevel.ERROR, message)
+
+    def fatal(self, message):
+        self.log(LogLevel.FATAL, message)
---
+++ system-design/low_level_design/uml.md
@@ -0,0 +1,14 @@
+Use case diagrams
+Class diagrams
+Sequence diagrams
+Activity diagrams
+
+Why use class diagrams?
+
+The following are some important purposes of the class diagram:
+
+- Represents the system's static structure.
+- Directly maps with object-oriented languages.
+- Represents what the system's duties or responsibilities are.
+
+Uses in both forward and reverse engineering.
