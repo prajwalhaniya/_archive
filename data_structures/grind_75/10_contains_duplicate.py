40fcd7f42057a990d26e1a08d2271f9d2a1b030b
prajwalhaniya <prajwalhaniya@gmail.com>
2025-01-29 23:47:10 +0530

    python practice

---
+++ data_structures/grind_75/10_contains_duplicate.py
@@ -0,0 +1,17 @@
+'''
+https://leetcode.com/problems/contains-duplicate
+'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+
+        return False
