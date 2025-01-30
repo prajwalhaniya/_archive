381659e92d639814f1459456622ed1478005aad4
prajwalhaniya <prajwalhaniya@gmail.com>
2025-01-30 23:52:52 +0530

    python practice

---
+++ data_structures/grind_75/12_meeting_rooms.py
@@ -0,0 +1,15 @@
+'''
+We're given an array of meeting intervals, intervals where intervals[i] = [start[i], end[i]]. 
+We have to determine if a person can attend all meetings without any overlap. 
+Return TRUE if possible, otherwise return FALSE.
+'''
+
+def can_attend_meetings(intervals):
+    
+    intervals.sort(key=lambda x: x[0])
+    
+    for i in range(len(intervals) - 1):
+        if intervals[i][1] > intervals[i + 1][0]:
+            return False
+    
+    return True
